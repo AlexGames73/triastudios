c02898b68b26d177b4234b89c4e6d3351907ea90
APGames <permenev.alex@ya.ru>
2021-01-12 14:14:45 +0400

    lab5

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,45 @@
 function onscroll() {
     if ($(document).scrollTop() < 100) {
-        $('.shadow').addClass('toppage');
-        $('.navbar').css('top', -$(document).scrollTop());
+        $('.navbar-my').css('top', 0);
     }
     else {
-        $('.shadow').removeClass('toppage');
-        $('.navbar')[0].animate({'top': 0}, 500, () => {});
+        $('.navbar-my').css('top', $(document).scrollTop());
     }
 }
 
-$(function() {
+$(() => {
+    $(document).on('click', 'a[href^="#"]', function(e) {
+        var id = $(this).attr('href');
+        var $id = $(id);
+        if ($id.length === 0) {
+            return;
+        }
+        e.preventDefault();
+        var pos = $id.offset().top;
+        $('body, html').animate({scrollTop: pos});
+    });
     $(document).on('scroll', onscroll);
     onscroll();
 });
+
+function submitSubscribe(e) {
+    e.preventDefault()
+    var form = new FormData(subscribeForm)
+    var email = form.get('email')
+    if (!subscribeForm.email.validity.valid) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Email is not valid!'
+        })
+    } else {
+        Swal.fire({
+            icon: 'success',
+            title: 'Congratulations',
+            text: "You have subscribed successfully!"
+        })
+        $('#exampleModal').modal('toggle')
+        console.log(email)
+    }
+    return false;
+}
